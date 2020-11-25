b7d8255f5075784e66e684fec639e960f0003935
Nivethika23 <74680760+Nivethika23@users.noreply.github.com>
2020-11-25 12:03:28 +0530

    Committed changes

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function clickvalue(){
+  incValue();
+  zeroValue();
+}
+  var i=0;
+  function incValue(){
+    i+=1;
+    document.getElementById("number").value=i;
+  }
+var i=0;
+function zeroValue(){
+i=0;
+  document.getElementById("number").value=i;
+}
